ad1111116cf0ab05f4bf4d71572199d0746e8378
Tomasz Prasołek <tomasz.prasolek@gmail.com>
2016-01-24 20:29:56 +0100

    Przykłady generowania listy obiektów

--- SimpleFixtures/Program.cs
+++ SimpleFixtures/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Ploeh.AutoFixture;
 using System;
+using System.Collections.Generic;
 
 namespace SimpleFixtures
 {
@@ -7,10 +8,26 @@ namespace SimpleFixtures
     {
         static void Main(string[] args)
         {
+            // 01. Pierwszy fixture
             Fixture fixture = new Fixture();
             var person = fixture.Create<Person>();
 
             Console.WriteLine(person);
+
+            // 02. Wygenerowanie listy obiektów
+
+            List<Person> list = new List<Person>(50);
+            for (int i = 0; i < 50; i++)
+            {
+                list.Add(fixture.Create<Person>());
+            }
+
+            // 03. Automatyczne wygenerowanie listy obiektów przez AutoFixture
+            int count = 50;
+            fixture.RepeatCount = 50;
+            List<Person> list2 = new List<Person>(count);
+            fixture.AddManyTo(list2);
+
             Console.ReadKey(true);
         }
     }
