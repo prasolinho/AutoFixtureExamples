134205fa27ce91bc552ade88ba39d59158c40a92
Tomasz Prasołek <tomasz.prasolek@gmail.com>
2016-01-24 17:23:50 +0100

    Pierwszy fixture klasy Person

---
+++ SimpleFixtures/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleFixtures
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Id: {0}, Name: {1}, Age: {2}", Id, Name, Age);
+        }
+    }
+}
---
+++ SimpleFixtures/Program.cs
@@ -0,0 +1,17 @@
+﻿using Ploeh.AutoFixture;
+using System;
+
+namespace SimpleFixtures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fixture fixture = new Fixture();
+            var person = fixture.Create<Person>();
+
+            Console.WriteLine(person);
+            Console.ReadKey(true);
+        }
+    }
+}
